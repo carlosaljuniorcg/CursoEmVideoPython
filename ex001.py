e0d1bf7c42cc3bd89af99b0b85dad887bfc7a9ec
Carlos Antônio <105462756+carlosaljuniorcg@users.noreply.github.com>
2022-05-18 16:19:49 -0300

    Estudos Python 001

---
+++ ex001.py
@@ -0,0 +1,10 @@
+"""print('Olá, mundo')
+msg = "Olá, mundo!!!"
+print(msg)
+nome = input("Digite seu nome: ")
+print("Seja bem vindo, ", nome)"""
+
+n1 = int(input('Digite um valor: '))
+n2 = int(input('Digite outro valor: '))
+s = n1 + n2
+print('A somos dos valores é', s)
