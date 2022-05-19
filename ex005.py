3a4455941e648243745b4db61ba3804a2a6092da
carlosaljuniorcg <carlosaljuniorcg@hotmail.com>
2022-05-19 15:47:12 -0300

    Novas 19.05

---
+++ ex005.py
@@ -0,0 +1,10 @@
+#Leia uma numero inteiro e mostre na tela o seu sucessor e antecessor
+n = int(input('Digite um número: '))
+#a = n - 1 
+#s = n + 1
+print('Analisando o seu valor {}, temos seu antecessor {} e seu sucessor {}.'. format(n, (n-1), (n+1)))
+
+#Dobro, triplo e raiz quadrada
+
+n = int(input('Digite um valor: '))
+print('O valor é {}, o dobro do valor é {} e a sua raiz quadrada é {}'.format(n, (n*2), (n**2)))
