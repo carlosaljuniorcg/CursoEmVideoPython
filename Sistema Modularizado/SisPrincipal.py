0bd3cbb7b60391789a43921202cceb7277f6ba65
Carlos Antônio <carlosaljuniorcg@hotmail.com>
2022-06-13 13:59:22 -0300

    Update 13062022

--- Sistema Modularizado/SisPrincipal.py
+++ Sistema Modularizado/SisPrincipal.py
@@ -14,8 +14,13 @@ while True:
         #Opção de listar o conteúdo de um arquivo!
         lerArquivo(arq)
     elif resposta == 2:
-        cabeçalho('Opção 2')
+        #Opção de cadastrar uma nova pessoa
+        cabeçalho('NOVO CADASTRO')
+        nome = str(input('Nome: '))
+        idade = leiaInt('Idade: ')
+        cadastrar(arq, nome, idade)
     elif resposta == 3:
+        #Opção de sair do sistema
         cabeçalho    ('Saindo do sistema... Até logo!')
         break
     else:
